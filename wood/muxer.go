8f405998decdcc5377deeb16153adeee03fc232d
indy <indy@mac4>
2023-02-20 17:12:49 -0800

    Log muxer

---
+++ wood/muxer.go
@@ -0,0 +1,52 @@
+package wood
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+// Log logs a message the provided text using the provided log level
+func Log(level logrus.Level, args ...interface{}) {
+	switch level {
+	case logrus.TraceLevel:
+		Trace(args)
+	case logrus.DebugLevel:
+		Debug(args)
+	case logrus.InfoLevel:
+		Info(args)
+	case logrus.WarnLevel:
+		Warn(args)
+	case logrus.ErrorLevel:
+		Error(args)
+	case logrus.FatalLevel:
+		Fatal(args)
+	case logrus.PanicLevel:
+		Panic(args)
+	}
+}
+
+// Logln logs a message with newline the provided log level
+func Logln(level logrus.Level, args ...interface{}) {
+	switch level {
+	case logrus.TraceLevel:
+		Traceln(args)
+	case logrus.DebugLevel:
+		Debugln(args)
+	case logrus.InfoLevel:
+		Infoln(args)
+	case logrus.WarnLevel:
+		Warnln(args)
+	case logrus.ErrorLevel:
+		Errorln(args)
+	case logrus.FatalLevel:
+		Fatalln(args)
+	case logrus.PanicLevel:
+		Panicln(args)
+	}
+}
+
+// Logf logs a message with formatting using the provided log level
+func Logf(level logrus.Level, args ...interface{}) {
+	decorateF(level, args, func(format string, args []any) {
+		std.Infof(format, args...)
+	})
+}
